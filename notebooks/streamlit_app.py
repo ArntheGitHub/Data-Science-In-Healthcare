bebd53e013f7049316ef90f56945c4e937daf49b
vantonheuri <123073572+vantonheuri@users.noreply.github.com>
2024-04-01 13:54:25 +0200

    v5

--- notebooks/streamlit_app.py
+++ notebooks/streamlit_app.py
@@ -2,26 +2,51 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
 # Load the CSV files to df
 df = pd.read_csv('data\cleaned\dataset_encoded.csv')
+df = pd.read_csv('data\cleaned\stroke_data_cleaned.csv')
+df = df.dropna()
 
 # Load the trained model and associated data
 with open("notebooks\Logistic_Regression.pkl", "rb") as f:
     trained_data = pickle.load(f)
 
 # Extract the model and other data
-# model = trained_data['model']
-# features = trained_data['features'] 
+model = trained_data['model']
+X_train = trained_data['X_train']
 
+
+# Function to create the interactive scatter plot
+def create_interactive_scatter_plot(df, target, features):
+    st.title('Interactive Scatter Plot')
+
+    # Sidebar for user input:
+    # User can select the x-axis for the scatter plot from features list
+    x_axis_options = features
+    x_axis = st.sidebar.selectbox('Select the x-axis for the scatter plot:', x_axis_options)
+
+    # Create a Plotly Express scatter plot
+    fig = px.scatter(df, x=x_axis, y=target,
+                     color=target, hover_data=features)
+    
+    # Update layout if desired
+    fig.update_layout(
+        title=f'Scatter Plot of {target.capitalize()} vs. {x_axis}',
+        xaxis_title=x_axis,
+        yaxis_title=target.capitalize()
+    )
+
+    # Display the plot
+    st.plotly_chart(fig, use_container_width=True)
 
 def predict_stroke_probability(features):
     # Scale the input features
     std_features = (features - X_train.mean(axis=0)) / X_train.std(axis=0)
     # Make predictions
     probability = model.predict_proba(std_features.reshape(1, -1))
-    #return probability[0][1]
-    pass
+    return probability[0][1]
 
 def main():
 
@@ -35,6 +60,19 @@ def main():
 
     st.write("### Data Overview", df.head())
     
+
+    # Define target and features
+    target = 'stroke'
+    features = ['hypertension', 'heart_disease', 'ever_married', 'Residence_type',
+                'avg_glucose_level', 'bmi', 'rounded_age', 'gender_Female',
+                'gender_Male', 'gender_Other', 'smoking_status_Unknown',
+                'smoking_status_formerly smoked', 'smoking_status_never smoked',
+                'smoking_status_smokes', 'work_type_Govt_job', 'work_type_Never_worked',
+                'work_type_Private', 'work_type_Self-employed', 'work_type_children']
+
+    # Call the function to create the interactive scatter plot
+    create_interactive_scatter_plot(df, target, features)
+
     
     st.write("### Data Features")
     # Input values for features
